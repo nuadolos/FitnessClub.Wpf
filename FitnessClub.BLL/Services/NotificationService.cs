3d62f4202bbad9bd1cef8711cc80fec9fc2a4494
nuadolos <2nuadolos1@gmail.com>
2023-03-29 23:43:51 +0300

    feat: added notification service

---
+++ FitnessClub.BLL/Services/NotificationService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace FitnessClub.BLL.Services;
+
+public static class NotificationService
+{
+    private const string ICON_ERROR_PATH = @"Images/error.png";
+    private const string ICON_WARNING_PATH = @"Images/warning.png";
+
+    private static ToastContentBuilder ToastContentWithArguments =>
+        new ToastContentBuilder()
+            .AddArgument("action", "viewConversation")
+            .AddArgument("conversationId", 9813)
+            .AddButton(new ToastButtonDismiss());
+
+    public static void NotifyInfo(string title, string description) =>
+        ToastContentWithArguments
+            .AddAppLogoOverride(new Uri(Path.GetFullPath(ICON_ERROR_PATH)))
+            .AddText(title)
+            .AddText(description)
+            .Show();
+
+    public static void NotifyError(string title, string description) =>
+        ToastContentWithArguments
+            .AddAppLogoOverride(new Uri(Path.GetFullPath(ICON_ERROR_PATH)))
+            .AddText(title)
+            .AddText(description)
+            .Show();
+
+    public static void NotifyWarning(string title, string description) =>
+        ToastContentWithArguments
+            .AddAppLogoOverride(new Uri(Path.GetFullPath(ICON_WARNING_PATH)))
+            .AddText(title)
+            .AddText(description)
+            .Show();
+}
