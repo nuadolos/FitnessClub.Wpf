78016e6aa1dcd70623d8de1e81a1a523f3c1ee34
nuadolos <2nuadolos1@gmail.com>
2023-03-29 23:44:50 +0300

    feat: added utils & styles

---
+++ FitnessClub.Desktop/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using FitnessClub.DAL;
+using FitnessClub.DAL.FitnessClubDataBase;
+using FitnessClub.DAL.FitnessClubDataBase.FakeData;
+using FitnessClub.Desktop.UI.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+
+namespace FitnessClub.Desktop;
+
+public partial class App : Application
+{
+    public static IHost AppHost { get; private set; } = null!;
+
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+                services.AddTransient<AuthorizationPage>();
+                services.AddTransient<RequestListPage>();
+
+                services.AddDALServices();
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        var context = AppHost.Services.GetRequiredService<FitnessClubContext>();
+        await context.Initialize();
+
+        var startupFrom = AppHost.Services.GetRequiredService<MainWindow>();
+        startupFrom.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
+}
---
+++ FitnessClub.Desktop/UI/Utilities/AppController.cs
@@ -0,0 +1,10 @@
+﻿using FitnessClub.DAL.FitnessClubDataBase.Entities.Consumers;
+using System.Windows.Controls;
+
+namespace FitnessClub.Desktop.UI.Utilities;
+
+public static class AppController
+{
+    public static Frame AppFrame { get; set; } = null!;
+    public static User? CurrentUser { get; set; }
+}
